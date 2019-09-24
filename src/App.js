111bcf6b5e0c6ace7d347f53c0d53fa1eda16610
HamidTanhaei <hamidt.com@gmail.com>
2019-09-24 15:52:05 +0330

    listen APP to redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import {store, persistor} from './redux/store';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        hellow world
+      </PersistGate>
+    </Provider>
   );
 }
 
