4671594d7484e12016ebcdb5d9cda2a2ff9c3168
HamidTanhaei <hamidt.com@gmail.com>
2019-09-25 10:56:21 +0330

    test Should search text change browser location

--- cypress/integration/searchField.spec.js
+++ cypress/integration/searchField.spec.js
@@ -1,4 +1,6 @@
 import {oneToOneJoinData} from '../../src/utils/joinRestData'
+import {browserRoutes} from '../../src/consts/browserRoutes';
+
 it('Should fetch by searched text', () => {
   cy.visit('/');
   const text = 'be';
@@ -23,4 +25,18 @@ it('Should fetch by searched text', () => {
         expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
       });
   }));
+});
+
+it('Should search text change browser location', () => {
+  cy.visit('/');
+  const text = 'be';
+  cy.get('.main-header .search input')
+    .type(text)
+    .should('have.value', text);
+
+  cy.get('.main-header .search .ant-input-suffix i svg').click();
+
+  cy.wait(300);
+
+  cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=1&search=' + text);
 });
