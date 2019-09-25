652ebd6987e9d4e5d9ebabc9ae1c50c61c089b04
HamidTanhaei <hamidt.com@gmail.com>
2019-09-25 11:39:27 +0330

    Feature Table Sort Change added(not completed)

--- cypress/integration/featuresTable.spec.js
+++ cypress/integration/featuresTable.spec.js
@@ -1,16 +1,12 @@
 import {oneToOneJoinData} from '../../src/utils/joinRestData';
+import {browserRoutes} from '../../src/consts/browserRoutes';
 
 describe('Page number Change', () => {
-
   it('Should fetch by change page number', () => {
     cy.visit('/');
-
     cy.get('.features-table .ant-table-pagination .ant-pagination-item-2').click();
-
     cy.get('.features-table .ant-spin-nested-loading .ant-spin-spinning').should('not.exist' );
-
     cy.wait(300);
-
     cy.get('.features-table tbody tr:first-child td:last-child').invoke('text').then((text => {
       cy.request({
         url: Cypress.env('api_server') + '/rights_and_roles_elements',
@@ -19,10 +15,97 @@ describe('Page number Change', () => {
           'page[number]': 2
         }
       }).then((response) => {
-        Cypress.log(response);
         expect(response.status).to.eq(200);
         expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
       });
     }));
+  });
+
+  it('Should location changes by change page number', () => {
+    cy.visit('/');
+    cy.get('.features-table .ant-table-pagination .ant-pagination-item-2').click();
+    cy.wait(300);
+    cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=2');
+  });
+
+  it('Should fetch based on browser location', () => {
+    const pageNumber = 2;
+    cy.visit(Cypress.env('host') + browserRoutes.listing + '?page[number]=' + pageNumber);
+    cy.get('.features-table .ant-spin-nested-loading .ant-spin-spinning').should('not.exist' );
+    cy.wait(300);
+    cy.get('.features-table tbody tr:first-child td:last-child').invoke('text').then((text => {
+      cy.request({
+        url: Cypress.env('api_server') + '/rights_and_roles_elements',
+        qs: {
+          'page[size]': 10,
+          'page[number]': pageNumber
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
+      });
+    }));
+  });
+});
+
+
+describe('Sort Change', () => {
+  it('Should fetch by sorted type (ascend)', () => {
+    cy.visit('/');
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.get('.features-table .ant-spin-nested-loading .ant-spin-spinning').should('not.exist' );
+    cy.wait(300);
+    cy.get('.features-table tbody tr:first-child td:last-child').invoke('text').then((text => {
+      cy.request({
+        url: Cypress.env('api_server') + '/rights_and_roles_elements',
+        qs: {
+          'page[size]': 10,
+          'page[number]': 1,
+          sort_type: 'label',
+          sort_direction: 'asc'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
+      });
+    }));
+  });
+
+  it('Should fetch by sorted type (descend)', () => {
+    cy.visit('/');
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.wait(300);
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.get('.features-table .ant-spin-nested-loading .ant-spin-spinning').should('not.exist' );
+    cy.wait(300);
+    cy.get('.features-table tbody tr:first-child td:last-child').invoke('text').then((text => {
+      cy.request({
+        url: Cypress.env('api_server') + '/rights_and_roles_elements',
+        qs: {
+          'page[size]': 10,
+          'page[number]': 1,
+          sort_type: 'label',
+          sort_direction: 'desc'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
+      });
+    }));
+  });
+
+  it('Should location changes by change sort', () => {
+    cy.visit('/');
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.wait(300);
+    cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=1&sort_type=label&sort_direction=asc');
+
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.wait(300);
+    cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=1&sort_type=label&sort_direction=desc');
+
+    cy.get('.features-table thead tr:first-child th:last-child .ant-table-column-sorter div[title=Sort]').click();
+    cy.wait(300);
+    cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=1');
   });
 });
--- cypress/integration/searchField.spec.js
+++ cypress/integration/searchField.spec.js
@@ -41,4 +41,22 @@ describe('Search Field', () => {
 
     cy.url().should('eq', Cypress.env('host') + browserRoutes.listing + '?page[number]=1&search=' + text);
   });
+
+  it('Should fetch based on browser location', () => {
+    const text = 'be';
+    cy.visit(Cypress.env('host') + browserRoutes.listing + '?search=' + text);
+    cy.get('.features-table .ant-spin-nested-loading .ant-spin-spinning').should('not.exist' );
+    cy.wait(300);
+    cy.get('.features-table tbody tr:first-child td:last-child').invoke('text').then((text => {
+      cy.request({
+        url: Cypress.env('api_server') + '/rights_and_roles_elements',
+        qs: {
+          search: text
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(oneToOneJoinData(response.body)[0]['element']['attributes']['label']).to.eq(text);
+      });
+    }));
+  });
 });
